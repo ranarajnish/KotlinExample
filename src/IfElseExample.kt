4e6879fa5542eacc7c99056affe584fb74bed380
Rana Rajnish <rana1385@outlook.com>
2025-01-21 06:16:53 +0530

    update

--- src/IfElseExample.kt
+++ src/IfElseExample.kt
@@ -5,9 +5,10 @@ fun main() {
     print("Enter two numbers->")
     val num1 = reader.nextInt()
     val num2 = reader.nextInt()
-    if (num1>num2){
-        print("$num1 is greater than $num2")
+    val result = if (num1>num2){
+        "$num1 is greater than $num2"
     }else{
-        println("$num1 is less than $num2")
+        "$num1 is less than $num2"
     }
+    print(result)
 }
